6c74a9958b1c2ab7d35d920ee9314924ed4b53c9
Priyanka Kumari <priyankacoder11@gmail.com>
2024-05-08 15:42:14 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,10 +25,10 @@ const Portfolio = () => {
         <div className="flex text-center justify-center gap-2 md:gap-0 md:flex-col text-[40px] md:text-[90px] 2xl:text-[120px] leading-[40px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase text-gradient mb-2">
           Latest Projects
         </div>
-        <div className="text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] text-[#CCCCCC] text-center">
+{/*         <div className="text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] text-[#CCCCCC] text-center">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry.
-        </div>
+        </div> */}
       </Div>
       {/* SECTION HEADING END */}
 
